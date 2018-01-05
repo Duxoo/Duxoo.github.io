8f71a67eddc4aa258779e45e271d47b0d815230a
Duxoo <32924675+Duxoo@users.noreply.github.com>
2018-01-05 16:26:16 +0300

    update
    adaptive

---
+++ main.js
@@ -0,0 +1,45 @@
+
+
+$(document).ready(function() {
+$('a[href^="#"]').click(function(){
+var el = $(this).attr('href');
+$('body').animate({
+scrollTop: $(el).offset().top}, 2000);
+return false;
+});
+});
+function go(){
+    alert("This article was not created yet");
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+function show(){
+    if(document.getElementById("main1").style.display=='flex')
+        {
+    document.getElementById("main1").style.display='none';
+    document.getElementById("row").style.display='none';
+    document.getElementById("grey").style.display='none';
+    document.getElementById("navig").style.display='block';
+        }
+    else{
+    document.getElementById("main1").style.display='flex';
+    document.getElementById("row").style.display='flex';
+    document.getElementById("grey").style.display='block';
+    document.getElementById("navig").style.display='none';
+    }
+
+}
